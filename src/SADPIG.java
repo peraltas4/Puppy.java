b50e5ab9736c6eca9d4932af15a44475390c7e03
Puppy <peraltas4@montclair.edu>
2024-01-22 22:14:49 -0500

    Puppy edit

--- src/SADPIG.java
+++ src/SADPIG.java
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//  Creating an Adorable lion  Author: Steffanie Peralta-Duran
+//  Creating an SAD PIG  Author: Steffanie Peralta-Duran
 //
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -10,16 +10,16 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 import javafx.stage.Stage;
 
-public class Puppy extends Application {
+public class SADPIG extends Application {
 
     //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    //  Presents an adorable Puppy
+    //  Presents an adorable PIG
     //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     public void start(Stage primaryStage)
 
     //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    //  Starting with the head of the puppy
+    //  Starting with the head of the PIG
     //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     {
 
@@ -35,14 +35,14 @@ public class Puppy extends Application {
 
 
         //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-        //  Creating the body of puppy
+        //  Creating the body of PIG
         //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
         Rectangle body = new Rectangle(100,180,100,70);
         body.setFill(Color.LIGHTPINK);
 
         //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-        //  Creating the arms and legs of puppy
+        //  Creating the arms and legs of PIG
         //-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
         Rectangle leftarm = new Rectangle(100,250,15,15);
@@ -67,7 +67,7 @@ public class Puppy extends Application {
 
 
         //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-        //  Creating the tail of the puppy
+        //  Creating the tail of the PIG
         //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
         Ellipse tail = new Ellipse(210,180,4,30);
@@ -103,7 +103,7 @@ public class Puppy extends Application {
         pedal3.setFill(Color.PLUM);
 
         Circle pedal4 = new Circle(320,203,12);
-        pedal4.setFill(Color.BLUE);
+        pedal4.setFill(Color.CORAL);
 
         Circle pedal5 = new Circle(308,221,12);
         pedal5.setFill(Color.DEEPPINK);
@@ -127,15 +127,65 @@ public class Puppy extends Application {
         Rectangle grass = new Rectangle(0,250,500,100);
         grass.setFill(Color.GREEN);
 
+        Line g1 = new Line(325,295,325,315);
+        g1.setFill(Color.DARKGREEN);
+        g1.setRotate(20);
+
+        Line g2 = new Line(320,295,320,315);
+        g2.setFill(Color.DARKGREEN);
+        g2.setRotate(-10);
+
+
+        Line g3 = new Line(310,295,310,315);
+        g3.setFill(Color.DARKGREEN);
+        g3.setRotate(-20);
+
+        Line g4 = new Line(315,295,315,315);
+        g4.setFill(Color.DARKGREEN);
+        g4.setRotate(-15);
+
+        Line g5 = new Line(375,290,375,305);
+        g5.setFill(Color.DARKGREEN);
+        g5.setRotate(-15);
+
+        Line g6 = new Line(380,290,380,305);
+        g6.setFill(Color.DARKGREEN);
+        g6.setRotate(-10);
+
+        Line g7 = new Line(385,290,385,305);
+        g7.setFill(Color.DARKGREEN);
+        g7.setRotate(-20);
+
+        Line g8 = new Line(390,290,390,305);
+        g8.setFill(Color.DARKGREEN);
+        g8.setRotate(25);
+
+        Line g9 = new Line(260,250,260,275);
+        g9.setFill(Color.DARKGREEN);
+        g9.setRotate(-20);
+
+        Line g10 = new Line(265,250,265,275);
+        g10.setFill(Color.DARKGREEN);
+        g10.setRotate(-10);
+
+
+        Line g11 = new Line(270,250,270,275);
+        g11.setFill(Color.DARKGREEN);
+        g11.setRotate(20);
+
+        Line g12 = new Line(270,250,275,275);
+        g12.setFill(Color.DARKGREEN);
+        g12.setRotate(-15);
+
 
 
         //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-        //  Grouping the puppy, tail, sun, and grass
+        //  Grouping the PIG, tail, sun, flower and grass
         //-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-        Group root = new Group(grass, sun,leftear, rightear, stem, head, smile, leftarm, leftleg, rightarm,pedal2,pedal6, pedal3, pedal4,pedal5, pedal7, rightleg, lefteye,righteye,body,tail, pedal1);
+        Group root = new Group(grass, sun,leftear,g1,g2,g3,g4,g5,g6,g7,g8,g9,g10,g11,g12, rightear, stem, head, smile, leftarm, leftleg, rightarm,pedal2,pedal6, pedal3, pedal4,pedal5, pedal7, rightleg, lefteye,righteye,body,tail, pedal1);
         Scene scene= new Scene(root,500, 350, Color.LIGHTCYAN);
-        primaryStage.setTitle("Sad Puppy");
+        primaryStage.setTitle("SAD PIG");
         primaryStage.setScene(scene);
         primaryStage.show();
 
